cef0e04b88b85caf5ffa07d98d30cb7ca068cd80
AlekseyKashubin <Akashubin@yahoo.com>
2023-05-16 15:03:33 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+var count = 0;
+var countElement = document.querySelector(".like-counter")
+function likeCounter(){
+    count++;
+    countElement.innerText = count + " like(s)";
+    console.log(count);
+}
+
+var count2 = 0;
+var countElement2 = document.querySelector(".like-counter2")
+function likeCounter2(){
+    count2++;
+    countElement2.innerText = count2 + " like(s)";
+    console.log(count2);
+}
+
+var count3 = 0;
+var countElement3 = document.querySelector(".like-counter3")
+function likeCounter3(){
+    count3++;
+    countElement3.innerText = count3 + " like(s)";
+    console.log(count3);
+}
